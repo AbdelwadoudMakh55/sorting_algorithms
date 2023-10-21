d3f3fd61d3b9b358b23ceb701417ae8c57b068bc
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-21 22:50:18 +0000

    Commit of task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,27 +6,34 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *next, *tmp_next, *tmp_tmp, *tmp;
+	listint_t *prev, *tmp_next, *tmp_prev, *tmp, *prev_prev;
 
 	if (list == NULL || *list == NULL)
 		return;
 	tmp = *list;
-	while ((*tmp).next != NULL)
+	while (tmp != NULL)
 	{
-		next = (*tmp).next;
-		while ((*tmp).n > (*next).n)
+		prev = (*tmp).prev;
+		while (prev != NULL)
 		{
-			tmp_next = (*next).next;
-			tmp_tmp = (*tmp).prev;
-			(*next).next = tmp;
-			(*tmp).prev = next;
-			(*tmp).next = tmp_next;
-			if (tmp_next != NULL)
-				(*tmp_next).prev = tmp;
-			(*next).prev = tmp_tmp;
-			tmp = next;
-			if ((*tmp).prev == NULL)
-				*list = tmp;
+			if ((*tmp).n < (*prev).n)
+			{
+				tmp_next = (*tmp).next;
+				tmp_prev = (*tmp).prev;
+				(*tmp).next = prev;
+				prev_prev = (*prev).prev;
+				if (prev_prev == NULL)
+					*list = tmp;
+				else
+					(*prev_prev).next = tmp;
+				(*tmp).prev = prev_prev;
+				(*prev).prev = tmp;
+				if (tmp_next != NULL)
+					(*tmp_next).prev = tmp_prev;
+				(*tmp_prev).next = tmp_next;
+				print_list(*list);
+			}
+			prev = (*prev).prev;
 		}
 		tmp = (*tmp).next;
 	}
