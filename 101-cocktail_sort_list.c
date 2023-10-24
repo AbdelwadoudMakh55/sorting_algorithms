31b3f1159d8313fcb71b55d44ca598d8447ceb09
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-24 12:10:05 +0000

    Cocktail sort betty fix

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,5 +1,71 @@
 #include "sort.h"
 /**
+ * forward_pass - Function for the forward pass of the cocktail sort.
+ * @list : Pointer to head of doubly linked list.
+ * @tmp : Temporary pointer for list traversal.
+ * Return: Void
+ */
+void forward_pass(listint_t **list, listint_t *tmp)
+{
+	listint_t *next, *next_next, *tmp_prev;
+
+	while ((*tmp).next != NULL)
+	{
+		next = (*tmp).next;
+		if ((*tmp).n > (*next).n)
+		{
+			next_next = (*next).next;
+			tmp_prev = (*tmp).prev;
+			if (tmp_prev != NULL)
+				(*tmp_prev).next = next;
+			else
+				*list = next;
+			(*next).prev = tmp_prev;
+			(*next).next = tmp;
+			(*tmp).prev = next;
+			(*tmp).next = next_next;
+			if (next_next != NULL)
+				(*next_next).prev = tmp;
+			print_list(*list);
+		}
+		else
+			tmp = (*tmp).next;
+	}
+}
+/**
+ * backward_pass - Function for the backward pass of the cocktail sort.
+ * @list : Pointer to head of doubly linked list.
+ * @tmp : Temporary pointer for list traversal.
+ * Return: Void.
+ */
+void backward_pass(listint_t **list, listint_t *tmp)
+{
+	listint_t *prev, *tmp_next, *prev_prev;
+
+	while ((*tmp).prev != NULL)
+	{
+		prev = (*tmp).prev;
+		if ((*tmp).n < (*prev).n)
+		{
+			tmp_next = (*tmp).next;
+			prev_prev = (*prev).prev;
+			if (prev_prev != NULL)
+				(*prev_prev).next = tmp;
+			else
+				*list = tmp;
+			(*tmp).next = prev;
+			(*tmp).prev = prev_prev;
+			(*prev).prev = tmp;
+			(*prev).next = tmp_next;
+			if (tmp_next != NULL)
+				(*tmp_next).prev = prev;
+			print_list(*list);
+		}
+		else
+			tmp = (*tmp).prev;
+	}
+}
+/**
  * listint_len - returns the number of elements in a linked listint_t list.
  * @h : Pointer to head of listint_t list.
  * Return: Number of nodes.
@@ -22,8 +88,7 @@ size_t listint_len(const listint_t *h)
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *init, *tmp, *prev, *next, *next_next, *tmp_prev, *tmp_next,
-	*prev_prev;
+	listint_t *init, *tmp;
 	size_t size, i = 0;
 
 	if (list == NULL || *list == NULL)
@@ -33,50 +98,8 @@ void cocktail_sort_list(listint_t **list)
 	while (i < size)
 	{
 		tmp = init;
-		while ((*tmp).next != NULL)
-		{
-			next = (*tmp).next;
-			if ((*tmp).n > (*next).n)
-			{
-				next_next = (*next).next;
-				tmp_prev = (*tmp).prev;
-				if (tmp_prev != NULL)
-					(*tmp_prev).next = next;
-				else
-					*list = next;
-				(*next).prev = tmp_prev;
-				(*next).next = tmp;
-				(*tmp).prev = next;
-				(*tmp).next = next_next;
-				if (next_next != NULL)
-					(*next_next).prev = tmp;
-				print_list(*list);
-			}
-			else
-				tmp = (*tmp).next;
-		}
-		while ((*tmp).prev != NULL)
-		{
-			prev = (*tmp).prev;
-			if ((*tmp).n < (*prev).n)
-			{
-				tmp_next = (*tmp).next;
-				prev_prev = (*prev).prev;
-				if (prev_prev != NULL)
-					(*prev_prev).next = tmp;
-				else
-					*list = tmp;
-				(*tmp).next = prev;
-				(*tmp).prev = prev_prev;
-				(*prev).prev = tmp;
-				(*prev).next = tmp_next;
-				if (tmp_next != NULL)
-					(*tmp_next).prev = prev;
-				print_list(*list);
-			}
-			else
-				tmp = (*tmp).prev;
-		}
+		forward_pass(list, tmp);
+		backward_pass(list, tmp);
 		i++;
 	}
 }
